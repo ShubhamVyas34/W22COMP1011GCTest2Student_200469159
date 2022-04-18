7d3311a8a879a7697277b0c7f33fee072e5aee3b
ShubhamVyas34 <shubhamvyas2687@gmail.com>
2022-04-18 08:56:31 -0400

    Question-2 Done

--- src/main/java/com/example/w22comp1011gctest2student/Product.java
+++ src/main/java/com/example/w22comp1011gctest2student/Product.java
@@ -1,4 +1,40 @@
 package com.example.w22comp1011gctest2student;
 
 public class Product {
+  public int id;
+  public String sku;
+  public String name;
+  public String salesPrice;
+  public String regularPrice;
+  public String image;
+
+  public int getId() {
+    return id;
   }
+
+  public String getSku() {
+    return sku;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getSalesPrice() {
+    return salesPrice;
+  }
+
+  public String getRegularPrice() {
+    return regularPrice;
+  }
+
+  public String getImage() {
+    return image;
+  }
+
+  @Override
+  public String toString() {
+    return " name=" + name + '\'' +
+            ", salesPrice='" + salesPrice + '\'' ;
+  }
+}
